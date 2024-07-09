3d1bfb3ffe3d611bfe20ae2e858e827d2cb65360
zhushanbo <zhushanbo@baidu.com>
2024-07-09 13:52:37 +0800

    no message

---
+++ test.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+// brew install node
+
+var hello = "Hello World!";
+console.log(hello)
+console.info('Hello JS')
+console.info('123')
+console.info('321')
+console.info('111')
